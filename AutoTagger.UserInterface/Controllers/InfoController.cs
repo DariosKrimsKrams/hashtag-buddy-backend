1ae3ad65d44d83c8ede9c6d9a38b21c007d37e92
Dario D. Müller <dario-domi@hotmail.de>
2018-05-23 17:55:15 +0200

    added /info/version Endpoint

---
+++ AutoTagger.UserInterface/Controllers/InfoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AutoTagger.UserInterface.Controllers
+{
+    public class InfoController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet("Info/Version")]
+        [ProducesResponseType(typeof(void), 200)]
+        public IActionResult Index()
+        {
+            var version = "v0.1";
+
+            var list = new Dictionary<string, string>();
+            list.Add("version", version);
+            return this.Json(list);
+        }
+    }
+}
