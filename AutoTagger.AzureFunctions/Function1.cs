269ce03e7900afb589e99388e49233315643a0b2
Dario D. Müller <dario-domi@hotmail.de>
2018-06-02 22:55:17 +0200

    working function that accepts a POST with an image

--- AutoTagger.AzureFunctions/Function1.cs
+++ AutoTagger.AzureFunctions/Function1.cs
@@ -9,22 +9,52 @@ using Newtonsoft.Json;
 
 namespace AutoTagger.AzureFunctions
 {
+    using System.Threading.Tasks;
+
     public static class Function1
     {
         [FunctionName("Function1")]
-        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
         {
-            log.Info("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            if (req.ContentType == null ||
+                !req.ContentType.Contains("multipart/form-data; boundary"))
+            {
+                return new BadRequestObjectResult("Wrong ContentType");
+            }
 
-            string requestBody = new StreamReader(req.Body).ReadToEnd();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            var body = req.Body;
+            if (body == null || body.Length == 0 || req.ContentLength == 0)
+            {
+                return new BadRequestObjectResult("No File uploaded");
+            }
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            //var storage = new MysqlUIStorage();
+            //services.AddTransient<, MysqlUIStorage>();
+            //services.AddTransient<ITaggingProvider, GCPVision>();
+
+            using (var stream = new MemoryStream())
+            {
+                await body.CopyToAsync(stream);
+                var bytes = stream.ToArray();
+
+                //var machineTags = this.taggingProvider.GetTagsForImageBytes(bytes).ToList();
+
+                //if (!machineTags.Any())
+                //{
+                //    return this.BadRequest("No MachineTags found :'(");
+                //}
+
+                //var content = this.FindTags(machineTags);
+                //var json = this.Json(content);
+
+                //var debug = content;
+                //var debugStr = JsonConvert.SerializeObject(debug);
+                //this.storage.Log("web_image", debugStr);
+
+                //return json;
+                return (ActionResult)new OkObjectResult($"Hello, WORKS :D");
+            }
         }
     }
 }
