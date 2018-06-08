22b9825724b6b70168ce616b59e85d36afb34e8c
Dario D. Müller <dario-domi@hotmail.de>
2018-06-08 23:20:30 +0200

    renamed Azure Function, added saveDebugInfos, updated dependencies, serialize Json without Newtonsoft.Json because problems with .net.sdsk.functions-newtonsoft.json

---
+++ AutoTagger.AzureFunctions/Upload.cs
@@ -0,0 +1,99 @@
+﻿namespace AutoTagger.AzureFunctions
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Runtime.Serialization.Json;
+    using System.Threading.Tasks;
+
+    using AutoTagger.Contract;
+    using AutoTagger.Database.Standard.Storage.Mysql;
+    using AutoTagger.ImageProcessor.Standard;
+
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Azure.WebJobs;
+    using Microsoft.Azure.WebJobs.Extensions.Http;
+    using Microsoft.Azure.WebJobs.Host;
+
+    public static class Upload
+    {
+        [FunctionName("Upload")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]
+            HttpRequest req,
+            TraceWriter log)
+        {
+            if (req.ContentType == null || !req.ContentType.Contains("multipart/form-data; boundary"))
+            {
+                return new BadRequestObjectResult("Wrong ContentType");
+            }
+
+            var body  = req.Body;
+            var files = req.Form.Files;
+            if (body == null || body.Length == 0 || req.ContentLength == 0 || files.Count != 1)
+            {
+                return new BadRequestObjectResult("No File uploaded");
+            }
+
+            var storage         = new MysqlUIStorage();
+            var taggingProvider = new GCPVision();
+
+            using (var stream = new MemoryStream())
+            {
+                await files[0].CopyToAsync(stream);
+                var bytes = stream.ToArray();
+
+                var machineTags = taggingProvider.GetTagsForImageBytes(bytes).ToList();
+
+                if (!machineTags.Any())
+                {
+                    return new BadRequestObjectResult("No MachineTags found");
+                }
+
+                var (query, instagramTags) = storage.FindHumanoidTags(machineTags);
+                var result = new Dictionary<string, object> { { "instagramTags", instagramTags } };
+
+                SaveDebugInfos(req, machineTags, instagramTags, query, storage);
+
+                return new JsonResult(result);
+            }
+        }
+
+        private static void SaveDebugInfos(
+            HttpRequest req,
+            List<IMTag> machineTags,
+            IEnumerable<string> instagramTags,
+            string query,
+            MysqlUIStorage storage)
+        {
+            var mTags = new List<string>();
+            foreach (var mTag in machineTags)
+            {
+                mTags.Add($"{{\"Name\":\"{mTag.Name}\",\"Score\":{mTag.Score},\"Source\":\"{mTag.Source}\"}}");
+            }
+
+            var ip = req.HttpContext.Connection?.RemoteIpAddress?.ToString();
+            var debugInfos = new Dictionary<string, List<string>>
+            {
+                { "machineTags", mTags },
+                { "instagramTags", instagramTags.ToList() },
+                { "query", new List<string> { query } },
+                { "ip", new List<string> { ip } },
+                { "backend_version", new List<string> { "0.2" } },
+            };
+            var json = SerializeJson(debugInfos);
+            storage.Log("web_image", json);
+        }
+
+        private static string SerializeJson(Dictionary<string, List<string>> dict)
+        {
+            var stream = new MemoryStream();
+            var ser    = new DataContractJsonSerializer(typeof(Dictionary<string, List<string>>));
+            ser.WriteObject(stream, dict);
+            stream.Position = 0;
+            var sr = new StreamReader(stream);
+            return sr.ReadToEnd();
+        }
+    }
+}
--- AutoTagger.Clarifai.Standard/ImageProcessorApp.cs
+++ AutoTagger.Clarifai.Standard/ImageProcessorApp.cs
@@ -1,7 +1,6 @@
-﻿using System;
-
-namespace AutoTagger.ImageProcessor.Standard
+﻿namespace AutoTagger.ImageProcessor.Standard
 {
+    using System;
     using System.Collections.Concurrent;
     using System.Collections.Generic;
     using System.Linq;
@@ -12,108 +11,62 @@ namespace AutoTagger.ImageProcessor.Standard
 
     public class ImageProcessorApp
     {
-        private static IImageProcessorStorage storage;
-        private static ITaggingProvider tagger;
-        private static ConcurrentQueue<IImage> queue;
-        private static ConcurrentQueue<IImage> dbSaveQueue;
-        public static Action<IImage> OnLookingForTags;
-        public static Action<IImage> OnFoundTags;
         public static Action<IImage> OnDbInserted;
-        public static Action OnDbSleep;
+
         public static Action OnDbSaved;
-        private static int taggerRunning;
-        private static int saveCounter;
-        private static readonly Random Random = new Random();
-        private static List<string> processed;
+
+        public static Action OnDbSleep;
+
+        public static Action<IImage> OnFoundTags;
+
+        public static Action<IImage> OnLookingForTags;
 
         private static readonly int ConcurrentThreadsLimit = 15;
+
+        private static readonly Random Random = new Random();
+
         private static readonly int SaveLimit = 5;
 
+        private static DbUsage currentDbUsage;
+
+        private static ConcurrentQueue<IImage> dbSaveQueue;
+
         private static IFileHandler fileHandler;
+
+        private static List<string> processed;
+
+        private static ConcurrentQueue<IImage> queue;
+
+        private static int saveCounter;
+
+        private static IImageProcessorStorage storage;
+
+        private static ITaggingProvider tagger;
+
+        private static int taggerRunning;
+
+        public ImageProcessorApp(IImageProcessorStorage db, ITaggingProvider taggingProvider)
+        {
+            storage     = db;
+            tagger      = taggingProvider;
+            dbSaveQueue = new ConcurrentQueue<IImage>();
+            queue       = new ConcurrentQueue<IImage>();
+            processed   = new List<string>();
+            fileHandler = new DiskFileHander();
+        }
 
         enum DbUsage
         {
             None,
+
             GetEntries,
+
             SaveThisFuckingShit
-        }
-        private static DbUsage currentDbUsage;
-
-        public ImageProcessorApp(IImageProcessorStorage db, ITaggingProvider taggingProvider)
-        {
-            storage = db;
-            tagger = taggingProvider;
-            dbSaveQueue = new ConcurrentQueue<IImage>();
-            queue = new ConcurrentQueue<IImage>();
-            processed = new List<string>();
-            fileHandler = new DiskFileHander();
-        }
-
-        public void Process()
-        {
-            new Thread(ImageProcessorApp.StartTaggerThreads).Start();
-            new Thread(ImageProcessorApp.InsertDb).Start();
-        }
-
-        private static void PrepareImages()
-        {
-            var files = fileHandler.GetAllUnusedImages();
-            var images = GetImagesWithoutMTags(files);
-            images.ForEach(i =>
-            {
-                if (processed.Contains(i.Shortcode))
-                    return;
-                queue.Enqueue(i);
-                processed.Add(i.Shortcode);
-            });
-
-            // bad performance -> better would be to run the Query other way
-            //this.MoveUsedFiles(files, images);
-        }
-
-        private static IEnumerable<IImage> GetImagesWithoutMTags(IList<string> files)
-        {
-            return storage.GetImagesWithoutMachineTags(files);
-        }
-
-        //private void MoveUsedFiles(string[] files, IEnumerable<IImage> images)
-        //{
-        //    var usedFiles = files.Where(x => !images.Any(y => y.Shortcode == x)).ToList();
-        //    foreach (var usedFile in usedFiles)
-        //    {
-        //        var path     = PathUnused + usedFile + Ext;
-        //        var pathUsed = PathUsed + usedFile + Ext;
-        //        File.Move(path, pathUsed);
-        //    }
-        //}
-
-        private static void StartTaggerThreads()
-        {
-            while (true)
-            {
-                for (var i = taggerRunning; i < ConcurrentThreadsLimit;i++)
-                {
-                    if(queue.TryDequeue(out IImage image))
-                    {
-                        new Thread(DoTaggerRequest).Start(image);
-                    }
-                    else
-                    {
-                        if (SetDbUsing(DbUsage.GetEntries))
-                        {
-                            PrepareImages();
-                            currentDbUsage = DbUsage.None;
-                        }
-                        break;
-                    }
-                }
-                Thread.Sleep(Random.Next(50, 150));
-            }
         }
 
         public static void DoTaggerRequest(object data)
         {
-            var image = (IImage)data;
+            var image = (IImage) data;
             Interlocked.Increment(ref taggerRunning);
             OnLookingForTags?.Invoke(image);
 
@@ -133,7 +86,7 @@ namespace AutoTagger.ImageProcessor.Standard
 
             try
             {
-                var path = fileHandler.GetFullPath(image.Shortcode);
+                var path  = fileHandler.GetFullPath(image.Shortcode);
                 var mTags = tagger.GetTagsForFile(path).ToList();
                 Interlocked.Decrement(ref taggerRunning);
                 if (!mTags.Any())
@@ -142,6 +95,7 @@ namespace AutoTagger.ImageProcessor.Standard
                     fileHandler.Delete(image.Shortcode);
                     return;
                 }
+
                 image.MachineTags = mTags;
                 dbSaveQueue.Enqueue(image);
                 Interlocked.Increment(ref saveCounter);
@@ -160,7 +114,6 @@ namespace AutoTagger.ImageProcessor.Standard
                 else
                 {
                     Console.WriteLine("Unknown Error");
-
                 }
             }
         }
@@ -176,9 +129,10 @@ namespace AutoTagger.ImageProcessor.Standard
                         storage.InsertMachineTagsWithoutSaving(image);
                         OnDbInserted?.Invoke(image);
                     }
+
                     storage.DoSave();
                     OnDbSaved?.Invoke();
-                    saveCounter = 0;
+                    saveCounter    = 0;
                     currentDbUsage = DbUsage.None;
                 }
                 else
@@ -190,16 +144,81 @@ namespace AutoTagger.ImageProcessor.Standard
             }
         }
 
+        public void Process()
+        {
+            new Thread(StartTaggerThreads).Start();
+            new Thread(InsertDb).Start();
+        }
+
+        private static IEnumerable<IImage> GetImagesWithoutMTags(IList<string> files)
+        {
+            return storage.GetImagesWithoutMachineTags(files);
+        }
+
+        private static void PrepareImages()
+        {
+            var files  = fileHandler.GetAllUnusedImages();
+            var images = GetImagesWithoutMTags(files);
+            images.ForEach(
+                i =>
+                {
+                    if (processed.Contains(i.Shortcode))
+                        return;
+                    queue.Enqueue(i);
+                    processed.Add(i.Shortcode);
+                });
+
+            // bad performance -> better would be to run the Query other way
+            //this.MoveUsedFiles(files, images);
+        }
+
         private static bool SetDbUsing(DbUsage newStatus)
         {
             if (currentDbUsage == DbUsage.None || currentDbUsage == newStatus)
             {
                 currentDbUsage = newStatus;
-                if(currentDbUsage == newStatus)
+                if (currentDbUsage == newStatus)
                     return true;
             }
+
             return false;
         }
 
+        //private void MoveUsedFiles(string[] files, IEnumerable<IImage> images)
+        //{
+        //    var usedFiles = files.Where(x => !images.Any(y => y.Shortcode == x)).ToList();
+        //    foreach (var usedFile in usedFiles)
+        //    {
+        //        var path     = PathUnused + usedFile + Ext;
+        //        var pathUsed = PathUsed + usedFile + Ext;
+        //        File.Move(path, pathUsed);
+        //    }
+        //}
+
+        private static void StartTaggerThreads()
+        {
+            while (true)
+            {
+                for (var i = taggerRunning; i < ConcurrentThreadsLimit; i++)
+                {
+                    if (queue.TryDequeue(out IImage image))
+                    {
+                        new Thread(DoTaggerRequest).Start(image);
+                    }
+                    else
+                    {
+                        if (SetDbUsing(DbUsage.GetEntries))
+                        {
+                            PrepareImages();
+                            currentDbUsage = DbUsage.None;
+                        }
+
+                        break;
+                    }
+                }
+
+                Thread.Sleep(Random.Next(50, 150));
+            }
+        }
     }
 }
