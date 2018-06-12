421352e80cf25dc881784098462995fc0bb71b0c
Dario D. Müller <dario-domi@hotmail.de>
2018-06-12 23:34:45 +0200

    reverted "refactored query not longer using of likes, comments and followerCount" (29th April 2018)

--- AutoTagger.Database.Standard/Storage/Mysql/MysqlUiStorage.cs
+++ AutoTagger.Database.Standard/Storage/Mysql/MysqlUiStorage.cs
@@ -20,20 +20,25 @@
 
         private string BuildQuery(IEnumerable<IMTag> machineTags)
         {
-            var limitTopPhotos    = 100;
+            var countInsertTags   = machineTags.Count();
+            var limitTopPhotos    = 200;
             var countTagsToReturn = 30;
             var whereConditionLabel = BuildWhereCondition(machineTags, "GCPVision_Label");
             var whereConditionWeb = BuildWhereCondition(machineTags, "GCPVision_Web");
 
-            var query = $"SELECT i.name, i.posts "
-                        + $"FROM itags as i LEFT JOIN photo_itag_rel as rel ON rel.itagId = i.id "
-                        + $"LEFT JOIN (SELECT p.id, count(m.name) as matches FROM photos as p "
-                        + $"LEFT JOIN mtags as m ON m.photoId = p.id "
-                        + $"WHERE (({whereConditionLabel}) AND m.source='GCPVision_Label')"
-                        + $"OR (({whereConditionWeb}) AND m.source='GCPVision_Web')"
-                        + $" GROUP BY p.id ORDER BY matches DESC LIMIT {limitTopPhotos} "
-                        + $") as sub2 ON sub2.id = rel.photoId WHERE sub2.id IS NOT NULL "
-                        + $"GROUP BY i.name ORDER by sum(matches) DESC LIMIT {countTagsToReturn}";
+            string query = $"SELECT i.name, i.posts "
+                         + $"FROM itags AS i LEFT JOIN photo_itag_rel AS rel ON rel.itagId = i.id LEFT JOIN "
+                         + $"( SELECT p.id, ((count(m.name)-2 * matches + {countInsertTags}) / (count(m.name) "
+                         + $"+ {countInsertTags} - matches)) *popularity as relationQuality "
+                         + $"FROM photos AS p LEFT JOIN mtags AS m ON m.photoId = p.id LEFT JOIN "
+                         + $"( SELECT p.id, (p.likes+p.comments)/ p.follower AS popularity, count(m.name) AS matches "
+                         + $"FROM photos as p LEFT JOIN mtags AS m ON m.photoId = p.id "
+                         + $"WHERE (({whereConditionLabel}) AND m.source='GCPVision_Label') "
+                         + $"OR (({whereConditionWeb}) AND m.source='GCPVision_Web') "
+                         + $"GROUP by p.id ORDER BY matches DESC LIMIT {limitTopPhotos}) AS sub1 ON p.id = sub1.id WHERE sub1.id IS NOT NULL "
+                         + $"GROUP by p.id ORDER BY relationQuality DESC LIMIT {limitTopPhotos} ) AS sub2 ON sub2.id = rel.photoId "
+                         + $"WHERE sub2.id IS NOT NULL GROUP by i.name ORDER by count(i.name) DESC, relationQuality DESC LIMIT {countTagsToReturn}";
+
             return query;
         }
 
