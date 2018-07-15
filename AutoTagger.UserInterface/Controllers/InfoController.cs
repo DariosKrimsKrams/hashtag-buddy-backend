1ace253ee10ba3a91ab346cd4a02c47cc1d7df51
Dario D. Müller <dario-domi@hotmail.de>
2018-07-15 16:29:28 +0200

    updated frontend

--- AutoTagger.UserInterface/Controllers/InfoController.cs
+++ AutoTagger.UserInterface/Controllers/InfoController.cs
@@ -11,6 +11,7 @@ namespace AutoTagger.UserInterface.Controllers
         public IActionResult Index()
         {
             var version = "v0.2";
+            var date = "2018-07-15";
 
             var list = new Dictionary<string, string>();
             list.Add("version", version);
