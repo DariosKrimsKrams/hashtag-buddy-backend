d7d1f7b655f4a2886d92e037921a2bcc3113b6d3
Dario D. Müller <dario-domi@hotmail.de>
2018-08-03 22:18:22 +0200

    added read csv logic

--- Instaq.BlacklistImport/CsvImporter.cs
+++ Instaq.BlacklistImport/CsvImporter.cs
@@ -1,17 +1,29 @@
 ﻿namespace Instaq.BlacklistImport
 {
-    using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Text.RegularExpressions;
 
     class CsvImporter
     {
         public IEnumerable<string> ReadFile(string filePath)
         {
-            //var fh = new FileHandler();
-            //var entries = fh.ReadCsv(filePath);
-            //return entries;
-            return new List<string>();
+            var entries = new List<string>();
+            using (var reader = new StreamReader(filePath))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line   = reader.ReadLine();
+                    var values = line.Split(',');
+                    foreach (var value in values)
+                    {
+                        if (string.IsNullOrEmpty(value))
+                            continue;
+                        entries.Add(value);
+                    }
+                }
+            }
+            return entries;
         }
     }
 }
