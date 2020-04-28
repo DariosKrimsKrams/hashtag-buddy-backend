b9b945c610d71f2d5cd750a13c54d606b3afe9ff
Dario D. Müller <dario-domi@hotmail.de>
2020-04-28 20:45:38 +0200

    no message

---
+++ Instaq.Database/Storage/Mysql/Generated/InstaqProdContextFactory.cs
@@ -0,0 +1,16 @@
+﻿namespace Instaq.Database.Storage.Mysql.Generated
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Design;
+
+    public class InstaqProdContextFactory : IDesignTimeDbContextFactory<InstaqProdContext>
+    {
+        public InstaqProdContext CreateDbContext(string[] args)
+        {
+            var connectionString = "Server=89.22.110.69;User Id=instaq_prod;Password=szV15D6mEeuT70GaOVP0GJ2N7NguJmRQztzoyMeqjTwtN1HC8ojC;Database=InstaqProd;TreatTinyAsBoolean=false;Convert Zero Datetime=True";
+            var optionsBuilder   = new DbContextOptionsBuilder<InstaqProdContext>();
+            optionsBuilder.UseMySql(connectionString);
+            return new InstaqProdContext(optionsBuilder.Options);
+        }
+    }
+}
