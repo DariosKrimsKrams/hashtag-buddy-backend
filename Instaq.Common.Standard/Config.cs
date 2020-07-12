581a5057aecd4b12c24c1d767b2e7d5af5401425
Dario D. Müller <dario-domi@hotmail.de>
2020-07-12 22:16:49 +0200

    changed version

--- Instaq.Common.Standard/Config.cs
+++ Instaq.Common.Standard/Config.cs
@@ -2,7 +2,7 @@
 {
     public static class Config
     {
-        public static int Version = 8;
-        public static string Date = "2020-05-06";
+        public static int Version = 9;
+        public static string Date = "2020-07-12";
     }
 }
